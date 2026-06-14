ba582a4086510c31564d46303527e12fdd242095
Florent Destremau <florent.destremau@gmail.com>
2026-06-15 01:56:13 +0200

    Add /up health endpoint pour kamal-proxy (once)

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthController extends AbstractController
+{
+    #[Route('/up', name: 'app_health', methods: ['GET', 'HEAD'])]
+    public function up(): Response
+    {
+        return new Response('OK', 200, ['Content-Type' => 'text/plain']);
+    }
+}
