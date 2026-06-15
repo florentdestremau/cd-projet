99e83427f96bb16b1cfe6b6215fb97d2ca35e906
Florent Destremau <florent.destremau@gmail.com>
2026-06-15 02:01:03 +0200

    Activate DoctrineFixturesBundle in all envs (auto-load au premier boot prod)

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
